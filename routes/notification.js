59f915da98da6dcbd7bfb837812a4a9553a930bc
Mridul Verma <94969636+mridul549@users.noreply.github.com>
2023-11-13 15:36:18 +0530

    Remove notification route module

--- routes/notification.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController')
-const notificationController = require('../controllers/notificationController')
-
-router.post('/notify', notificationController.sendNotification)
-
-module.exports = router;
